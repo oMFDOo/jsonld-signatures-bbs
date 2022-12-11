76b650cfe436c79f2df9fcd53beee1bb008f0223
oMFDOo <50069569+oMFDOo@users.noreply.github.com>
2022-12-11 22:17:33 +0900

    issuer 복제

--- sample/browser/index.web-sample.js
+++ sample/browser/index.web-sample.js
@@ -23,11 +23,15 @@ import {
 import { extendContextLoader, sign, verify, purposes } from "jsonld-signatures";
 
 import inputDocument from "./data/inputDocument.json";
+import inputDocument2 from "./data/inputDocument2.json";
 import keyPairOptions from "./data/keyPair.json";
+import keyPairOptions2 from "./data/keyPair2.json";
 import exampleControllerDoc from "./data/controllerDocument.json";
+import exampleControllerDoc2 from "./data/controllerDocument2.json";
 import bbsContext from "./data/bbs.json";
 // ProofFrame
 import revealDocument from "./data/deriveProofFrame.json";
+import revealDocument2 from "./data/deriveProofFrame.json";
 import citizenVocab from "./data/citizenVocab.json";
 import credentialContext from "./data/credentialsContext.json";
 // Jwk, Jws ~
@@ -44,20 +48,51 @@ const documents = {
   "https://www.w3.org/2018/credentials/v1": credentialContext,
   "https://w3id.org/security/suites/jws-2020/v1": suiteContext,
 };
-
+const documents2 = {
+  "did:example:489398593#test": keyPairOptions2,
+  "did:example:489398593": exampleControllerDoc2,
+  "https://w3id.org/security/bbs/v1": bbsContext,
+  "https://w3id.org/citizenship/v1": citizenVocab,
+  "https://www.w3.org/2018/credentials/v1": credentialContext,
+  "https://w3id.org/security/suites/jws-2020/v1": suiteContext,
+};
 /* eslint-disable-next-line @typescript-eslint/no-explicit-any */
-const customDocLoader = (url) => {
-  const context = documents[url];
 
-  if (context) {
-    return {
-      // 이것은 링크 헤더를 통한 컨텍스트를 위한 것입니다.
-      contextUrl: null, // this is for a context via a link header
-      // 이것은 로드된 실제 문서입니다.
-      document: context, // this is the actual document that was loaded
-      // 리디렉션 후의 실제 컨텍스트 URL입니다.
-      documentUrl: url, // this is the actual context URL after redirects
-    };
+
+var cnt = 0;
+
+const customDocLoader = (url) => {
+  console.log("카운팅 쓰타", cnt, cnt%2==0);
+  if (cnt % 2 == 0) {
+    const context = documents[url];
+    //console.log("url1 : ", url);
+  
+    if (context) {
+      return {
+        // 이것은 링크 헤더를 통한 컨텍스트를 위한 것입니다.
+        contextUrl: null, // this is for a context via a link header
+        // 이것은 로드된 실제 문서입니다.
+        document: context, // this is the actual document that was loaded
+        // 리디렉션 후의 실제 컨텍스트 URL입니다.
+        documentUrl: url, // this is the actual context URL after redirects
+      };
+    }
+  }
+  else {
+    const context = documents2[url];
+    //console.log("url2 : ", url);
+  
+    if (context) {
+      return {
+        // 이것은 링크 헤더를 통한 컨텍스트를 위한 것입니다.
+        contextUrl: null, // this is for a context via a link header
+        // 이것은 로드된 실제 문서입니다.
+        document: context, // this is the actual document that was loaded
+        // 리디렉션 후의 실제 컨텍스트 URL입니다.
+        documentUrl: url, // this is the actual context URL after redirects
+      };
+    }
+
   }
 
   console.log(
@@ -68,38 +103,57 @@ const customDocLoader = (url) => {
   );
 };
 
+
+
 //Extended document load that uses local contexts
-/* eslint-disable-next-line @typescript-eslint/no-explicit-any */
 //로컬 컨텍스트를 사용하는 확장 문서 로드
 /* eslint-disable-next-line @typescript-eslint/no-explicit-any */
-const documentLoader = extendContextLoader(customDocLoader);
 
 
 const main = async () => {
+  const documentLoader = extendContextLoader(customDocLoader);
+  console.log("이고얌 : ", documentLoader);
+
   //Import the example key pair
   // 예제 키 쌍 가져오기
   const keyPair = await new Bls12381G2KeyPair(keyPairOptions);
-  let c = JSON.stringify(keyPair.toString(), null, 2).toString().replace(/"publicKey":"oq/g, '"publicKey":"fe');
-  let myKey = keyPair;
-  console.log("변경 전 : ", keyPair);
-  // myKey.publicKey = myKey.publicKey.toString().replace(/R/g, 's');
-  console.log("변경 후 : ", JSON.stringify(c, null, 2));
+  const keyPair2 = await new Bls12381G2KeyPair(keyPairOptions2);
 
-  console.log("Input document : 기본 VC");
+  console.log("Input document : 기본 VC1");
   console.log(JSON.stringify(inputDocument, null, 2));
+  console.log("Input document : 기본 VC2");
+  console.log(JSON.stringify(inputDocument2, null, 2));
 
   //Sign the input document
-  //입력 문서에 서명
-  // keyPair : Bls12381G2KeyPair
+  // 입력 문서에 서명 : Issuer1
   let signedDocument = await sign(inputDocument, {
     suite: new BbsBlsSignature2020({ key: keyPair }),
     purpose: new purposes.AssertionProofPurpose(),
     documentLoader,
   });
+  cnt++;
 
-  console.log("Input document with proof : Issuer 서명 결과");
+  // 입력 문서에 서명 : Issuer2
+  let signedDocument2 = await sign(inputDocument2, {
+    suite: new BbsBlsSignature2020({ key: keyPair2 }),
+    purpose: new purposes.AssertionProofPurpose(),
+    documentLoader,
+  });
+  cnt++;
+  
+
+
+  console.log("Input document with proof : Issuer1 서명 결과");
   console.log("type : " + typeof(signedDocument));
   console.log(JSON.stringify(signedDocument, null, 2));
+  
+  console.log("Input document with proof : Issuer2 서명 결과");
+  console.log("type : " + typeof(signedDocument2));
+  console.log(JSON.stringify(signedDocument2, null, 2));
+
+  
+  /* #region VC 변형 테스트 */
+
   // VC 변형 테스트 1 - 데이터 변형
   // let change = JSON.stringify(signedDocument, null, 2).toString().replace(/Jinju/g, 'Chacha');
   // console.log(change);
@@ -113,38 +167,56 @@ const main = async () => {
   // change = JSON.parse(change);
   // signedDocument = change;
 
+  /* #endregion */
+
   //Verify the proof
-  //증명 확인
+  //증명 확인 : Issuer1
   let verified = await verify(signedDocument, {
     suite: new BbsBlsSignature2020(),
     purpose: new purposes.AssertionProofPurpose(),
     documentLoader,
   });
+  cnt++;
+
+  //증명 확인 : Issuer2
+  let verified2 = await verify(signedDocument2, {
+    suite: new BbsBlsSignature2020(),
+    purpose: new purposes.AssertionProofPurpose(),
+    documentLoader,
+  });
+  cnt++;
 
 
-  console.log("Verification result : 일반 VC 검증 결과");
+  console.log("Verification result : Issuer1의 일반 VC 검증 결과");
   console.log(JSON.stringify(verified, null, 2));
 
+  console.log("Verification result : Issuer2의 일반 VC 검증 결과");
+  console.log(JSON.stringify(verified2, null, 2));
+
+
   //Derive a proof
-  //증명 도출 : 서명 확장==> VP
+  // 증명 도출 : Issuer1의 VC를 이용한 서명 확장 ==> VP
   let derivedProof = await deriveProof(signedDocument, revealDocument, {
     suite: new BbsBlsSignatureProof2020(),
     documentLoader,
   });
-  
-  console.log("Verifying Derived Proof : 파생 증명 확인");
-  console.log(JSON.stringify(derivedProof, null, 2));
+  cnt++;
 
-  // console.log("키생성")
-  // var myKey = generateKeyPair({
-  //   bits: 2048,
-  //   e: 0x10001,
-  //   workers: -1
-  // }, (err, keyPair) => {
-  // });
-  // console.log("공개키", myKey.publicKey);
-  // console.log("비밀키", myKey.privateKey);
-    
+  // 증명 도출 : Issuer2의 VC를 이용한 서명 확장 ==> VP
+  let derivedProof2 = await deriveProof(signedDocument2, revealDocument2, {
+    suite: new BbsBlsSignatureProof2020(),
+    documentLoader,
+  });
+  cnt++;
+  
+  console.log("Verifying Derived Proof : Issuer1을 이용한 파생 증명 확인");
+  console.log(JSON.stringify(derivedProof, null, 2));
+  console.log("Verifying Derived Proof : Issuer2를 이용한 파생 증명 확인");
+  console.log(JSON.stringify(derivedProof2, null, 2));
+
+
+  /* #region VP 변형 테스트 */
+ 
   // VP 변형 테스트 2
   // let change = JSON.stringify(derivedProof, null, 2).toString().replace(/Jinju/g, 'Chacha');
   // console.log(change);
@@ -152,11 +224,11 @@ const main = async () => {
   // derivedProof = change;
   
   // VC 변형 테스트 2 - 데이터 누락
-  let change = JSON.stringify(derivedProof, null, 2).toString().replace(/"gender": "Female",/g, '');
-  console.log("변형 내용");
-  console.log(change);
-  change = JSON.parse(change);
-  derivedProof = change;
+  // let change = JSON.stringify(derivedProof, null, 2).toString().replace(/"gender": "Female",/g, '');
+  // console.log("변형 내용");
+  // console.log(change);
+  // change = JSON.parse(change);
+  // derivedProof = change;
 
   // Issuer DID 변경
   // let change = JSON.stringify(derivedProof, null, 2).toString().replace(/489398593/g, '489398585');
@@ -164,16 +236,27 @@ const main = async () => {
   // change = JSON.parse(change);
   // derivedProof = change;
 
+  /* #endregion */
+
   //Verify the derived proof
-  // 파생된 증명 확인
+  // Issuer 1의 파생된 증명 확인
   verified = await verify(derivedProof, {
     suite: new BbsBlsSignatureProof2020(),
     purpose: new purposes.AssertionProofPurpose(),
     documentLoader,
   });
+  // Issuer 2의 파생된 증명 확인
+  verified = await verify(derivedProof2, {
+    suite: new BbsBlsSignatureProof2020(),
+    purpose: new purposes.AssertionProofPurpose(),
+    documentLoader,
+  });
 
-  console.log("Verification result : derivedProof 검증 결과");
+  console.log("Verification result : VP1의 derivedProof 검증 결과");
   console.log(JSON.stringify(verified, null, 2));
+
+  console.log("Verification result : VP2의 derivedProof 검증 결과");
+  console.log(JSON.stringify(verified2, null, 2));
 };
 
 main();
